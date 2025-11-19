b2a4fc7b5e4800935652ace87781a6ba25db01e7
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-11-19 14:42:12 +0000

    i tried to make a bar chart

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("hello world")
+import pandas as pd
+from matplotlib import pyplot as plt
 
+# Set style theme
+plt.style.use('seaborn-v0_8-pastel')
+print(plt.style.available)
 
-if __name__ == "__main__":
-    main()
+# Read CSV into DataFrame
+df = pd.read_csv('class-data-v1.csv')
+print(df.info())
+
+# Bar charts are great for COUNTS
+# often use String data for the labels
+df_sorted = df.sort_values(by='', ascending=False)
+
+colors = ['']
+plt.bar(df_sorted['Name'], df_sorted['WebDev Rating'], color=colors, width=0.6)
+
+# Often need to rotate labels on x acis for readability
+plt.xticks(rotation=45)
+plt.savefig('barchart.png', bbox_inches='tight')
+plt.close()
