3b82a3f158fd957c97d577788d4504398d6ca5fd
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-11-20 13:56:41 +0000

    we made a catplot instead bc bar charts arent good enough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 
 # Set style theme
@@ -9,14 +10,22 @@ print(plt.style.available)
 df = pd.read_csv('class-data-v1.csv')
 print(df.info())
 
-# Bar charts are great for COUNTS
-# often use String data for the labels
-df_sorted = df.sort_values(by='', ascending=False)
+barWidth = 0.2
+r1 = np.arange(8)
+r2 = r1 + barWidth
+r3 = r2 + barWidth
 
-colors = ['']
-plt.bar(df_sorted['Name'], df_sorted['WebDev Rating'], color=colors, width=0.6)
+colors = df['Hex Color'].dropna().tolist()
 
-# Often need to rotate labels on x acis for readability
-plt.xticks(rotation=45)
+fig, ax = plt.subplots(dpi=300)
+ax.bar(r1, df['Webdev Rating'].dropna(), color=colors, width=barWidth, edgecolor='#ffffff', label='WebDev')
+ax.bar(r2, df['Java Rating'].dropna(), color=colors, width=barWidth, edgecolor='#ffffff', label='Java')
+ax.bar(r3, df['Python Rating'].dropna(), color=colors, width=barWidth, edgecolor='#ffffff', label='Python}')
+
+
+ax.set_xticks(r1 + barWidth)
+ax.set_xticklabels(df['Name'].head(8))
+ax.legend() # TODO: fix legend to show language names 
+plt.title('Programming Language Ratings by Student')
 plt.savefig('barchart.png', bbox_inches='tight')
 plt.close()
